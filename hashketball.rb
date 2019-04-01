bf5541084fbfdf1ef9880a4f2c9794be94042036
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-04-01 21:40:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -118,7 +118,8 @@ def game_hash
 end 
 
 def num_points_scored(players_name)
-  game_hash[:away][:players].find 
+  game_hash[:away][:players].find do |player_hash|
+    
   # game_hash.each do |team, players|
   #   players[:players].each do |name, info|
   #     if name == players_name
